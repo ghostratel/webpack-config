500d9fbc5b18114d03b90efecb00e4f92bc964f1
ghostratel <1353640486@qq.com>
2018-07-08 23:03:55 +0800

    seems ok

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
 # webpack-config
 
-## 一个基于webpack4的简单配置。
-
-配置有热更新，支持es6，eslint语法检查。
-
-`npm run start`
-启动开发服务器
-
-`npm run build`
-编译打包
+## 自用webpack
--- webpack.config.js
+++ webpack.config.js
@@ -1,99 +1,43 @@
 const path = require('path')
-const glob = require('glob')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const PurifyCSSPlugin = require('purifycss-webpack')
-const env = process.argv[3]
-const webpack = require('webpack')
+const pluginsConfig = require('./webpack.plugins.js')
+const rulesConfig = require('./webpack.rules.js')
 
 module.exports = {
-  entry: { // 入口文件(可配置多入口)
-    index: './src/index.js'
-    // login: './src/login.js'
+  entry: {
+    // 多入口文件
+    index: './src/index.js',
+    index2: './src/index2.js'
+    // jquery: 'jquery'
   },
   output: {
-    filename: '[name].js', // 打包后的文件名称
-    path: path.resolve('dist') // 打包后的目录，必须是绝对路径
+    path: path.resolve(__dirname, 'dist'),
+    // 打包多出口文件
+    // 生成 a.bundle.js  b.bundle.js  jquery.bundle.js
+    filename: './js/[name].bundle.js'
   },
+  plugins: pluginsConfig,
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
-    historyApiFallback: true,
-    port: 8888,
-    open: true
+    host: 'localhost',
+    port: '8080',
+    open: true, // 开启浏览器
+    hot: true // 开启热更新
   },
-  plugins: [
-    new CleanWebpackPlugin('dist'), // 打包前先清空
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      hash: true,
-      filename: 'index.html',
-      chunks: ['index'] // 对应关系,index.js对应的是index.html
-    }),
-    /*
-    * 多页面按照此规则进行配置即可
-    * template, filename以及chunks名称需要一一对应
-    */
-    // new HtmlWebpackPlugin({
-    //   template: './src/login.html',
-    //   hash: true,
-    //   filename: 'login.html',
-    //   chunks: ['login'] // 对应关系,login.js对应的是login.html
-    // }),
-    new PurifyCSSPlugin({
-      // Give paths to parse for rules. These should be absolute!
-      paths: glob.sync(path.join(__dirname, 'src/*.html'))
-    }),
-    // 拆分后会把css文件放到dist目录下的css/style.css
-    new ExtractTextWebpackPlugin('css/[name].css'),
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  // devtool: "source-map",  // 开启调试模式
   module: {
-    rules: [{
-      test: /\.(css|scss)$/,
-      use: ExtractTextWebpackPlugin.extract({
-        use: [{
-          loader: 'css-loader',
-          options: {
-            // 开发环境就不压缩css
-            minimize: env === 'development' ? !env === 'development' : true
-          }
-        }, {
-          loader: 'postcss-loader'
-        }, {
-          loader: 'sass-loader'
-        }],
-        fallback: 'style-loader'
-      })
-    }, {
-      test: /\.js$/,
-      use: 'bable-loader',
-      include: '/src/',
-      exclude: '/node_modules/'
-    }, {
-      test: /\.js$/,
-      loader: 'eslint-loader',
-      enforce: 'pre',
-      include: [path.resolve(__dirname, 'src')],
-      options: {
-        formatter: require('eslint-friendly-formatter')
-      }
-    }, {
-      test: /\.(jpe?g|png|gif)$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 8192, // 小于8k的图片自动转成base64格式，并且不会存在实体图片
-          outputPath: 'images/' // 图片打包后存放的目录
-        }
-      }]
-    }, {
-      test: /\.(htm|html)$/i,
-      use: ['html-withimg-loader']
-    }, {
-      test: /\.(eot|ttf|woff|svg)$/,
-      use: ['file-loader']
-    }]
+    rules: rulesConfig
   },
-  mode: 'development'
+  // 提取js，lib1名字可改
+  optimization: {
+    splitChunks: {
+      // cacheGroups: {
+      //   common: {
+      //     chunks: 'initial',
+      //     name: 'jquery',
+      //     enforce: true
+      //   }
+      // }
+    }
+  }
+
 }
