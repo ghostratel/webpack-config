a42388e3cad4a870fb538694b0184b574d4f5bcc
fenggang <1353640486@qq.com>
2019-03-25 17:42:56 +0800

    初始化项目

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import {sum} from './sum'
+var minus = require('./minus')
+
+console.log(sum(1,2));
+
+console.log(minus(4,1));
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].[hash:5].js'
+  }
+}
