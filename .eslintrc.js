a564e6e55e9c48b61140a1b878b48ce9c82d7282
冯罡 <1353640486@qq.com>
2019-12-22 13:37:12 +0800

    use eslint-typescript instead of tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,18 @@
 module.exports = {
+    "root": true,
     'env': {
         'browser': true,
         'es6': true,
         'node': true
     },
-    'extends': [
-        'eslint:recommended'
-    ],
     'globals': {
         'Atomics': 'readonly',
         'SharedArrayBuffer': 'readonly',
         'DEV': true
     },
-    "parser": "babel-eslint",
     "parserOptions": {
-      "sourceType": "module",
-      "allowImportExportEverywhere": true
-    },
-    'settings': {
+        "sourceType": "module",
+        "allowImportExportEverywhere": true
     },
     'rules': {
         'indent': [
@@ -37,6 +32,27 @@ module.exports = {
             'never'
         ],
         'no-console': 'off',
-        'no-debugger': 'off'
-    }
+        'no-debugger': 'off',
+        "space-in-parens": ["error", "never"]
+    },
+    "overrides": [
+        {
+            "files": ["*.ts", "*.tsx"],
+            "parser": "@typescript-eslint/parser",
+            "extends": [
+                'plugin:@typescript-eslint/eslint-recommended',
+                'plugin:@typescript-eslint/recommended'
+            ],
+            "plugins": [
+                "@typescript-eslint"
+            ],
+        },
+        {
+            "files": ["*.js", "*.jsx"],
+            "parser": "babel-eslint",
+            "extends": [
+                'eslint:recommended'
+            ]
+        }
+    ]
 }
--- src/entries/testTypescript.ts
+++ src/entries/testTypescript.ts
@@ -1,7 +1,9 @@
 import {randomNumber} from '../modules/tsModule'
+import hello from '../modules/test.js'
 
 for(let i = 0; i < 100; i++) {
-  console.log(randomNumber(0, 10))
+  console.log(randomNumber(0, 1))
+  hello()
 }
 
 
